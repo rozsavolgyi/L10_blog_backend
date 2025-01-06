10c1ddb0309ed69a486d566b547fa3a210588cd2
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-01-06 12:31:06 +0100

    aa

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request){
-        return $request;
+        $validator= Validator::make(
+            $request->all(),
+            [
+                'name'=>'required',
+                'email'=>'required|email|unique:App\Models\user,email',
+                'password'=>'required',
+                'role'=> 'required|integer'
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json($validator->errors(),400);
+        }
+
+        $input=$request->all();
+        $input['password']=bcrypt($input['password']);
+        $user= User::create($input);
+        return response()->json($input,201);
     }
 }
